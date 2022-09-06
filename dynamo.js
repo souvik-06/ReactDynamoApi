8b25ef10d52bdcef7fdf8ba8ad1b78dde291cbc8
anuragchhetri <anurag.chhetri@nagarro.com>
2022-09-06 12:33:42 +0530

    Dynamo Database Api

---
+++ dynamo.js
@@ -0,0 +1,52 @@
+const AWS = require('aws-sdk');
+require('dotenv').config();
+AWS.config.update({
+    region: "ap-south-1",
+    endpoint: "http://localhost:8000",
+});
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = 'users';
+const getCharacters = async () => {
+    const params = {
+        TableName: TABLE_NAME,
+    };
+    const characters = await dynamoClient.scan(params).promise();
+    return characters;
+};
+
+const getCharacterById = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            email_id: id,
+        },
+    };
+    return await dynamoClient.get(params).promise();
+};
+
+const addOrUpdateCharacter = async (character) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Item: character,
+    };
+    return await dynamoClient.put(params).promise();
+};
+
+const deleteCharacter = async (id) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            id,
+        },
+    };
+    return await dynamoClient.delete(params).promise();
+};
+
+module.exports = {
+    dynamoClient,
+    getCharacters,
+    getCharacterById,
+    addOrUpdateCharacter,
+    deleteCharacter,
+};
